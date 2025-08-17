81a18365b8218d7beef6e5838e0d70bfcd7cb6c4
TheBertusoCompany <dinj@bertuso.com>
2025-08-17 12:58:31 -0500

    Add /beta private page + robots.txt (prevent indexing)

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -1,18 +1,8 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://6ef854978d59df74255600f51fc849c5@o4509833938862080.ingest.us.sentry.io/4509833942007808",
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN || "",
   tracesSampleRate: 1,
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
