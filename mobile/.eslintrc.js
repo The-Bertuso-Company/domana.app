2b96ccb9fadddcecdb07c284625f888935dcadd5
TheBertusoCompany <dinj@bertuso.com>
2025-09-12 13:03:52 -0500

    chore(config): finalize Step 47 (scripts fixed, TS sentinel, ESLint extends)

---
+++ mobile/.eslintrc.js
@@ -0,0 +1,19 @@
+﻿module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
+  extends: [
+    "expo",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    "prettier/prettier": ["error"]
+  },
+  env: {
+    "react-native/react-native": true,
+    es2021: true
+  }
+};
+
